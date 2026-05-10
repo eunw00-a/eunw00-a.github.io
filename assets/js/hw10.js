d8fc00a0352c32337554d5c3429888024a16b160
eunw00-a <eunw00@snu.ac.kr>
2026-05-10 16:31:24 +0000

    글자 세기 함수

--- assets/js/hw10.js
+++ assets/js/hw10.js
@@ -33,10 +33,25 @@ const titled = works.map(work => `「${work}」`);
 console.log(titled);
 
 const long = works.filter(work => work.length >= 3);
-console.log(long)
+console.log(long);
 
 let n = 1;
 for (const work of long) {
     console.log(`${n}번째 작품: ${work}`);
     n++;
 }
+
+// Q3
+function countChar(text, target) {
+    let count = 0;
+    for (const ch of text) {
+        if (ch===target) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(`"박씨는 이씨에게 시집간 김씨의 외사촌 동생이다."에서 '씨'는 ${countChar("박씨는 이씨에게 시집간 김씨의 외사촌 동생이다.", "씨")}번 등장합니다.`);
+console.log(`"이상의 「날개」는 1936년 작품이다."에서 '이'는 ${countChar("이상의 「날개」는 1936년 작품이다.", "이")}번 등장합니다.`);
+console.log(`"banana"에서 'a'는 ${countChar("banana", "a")}번 등장합니다.`)
