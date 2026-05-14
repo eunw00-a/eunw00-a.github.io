2d0a6e269326931da94841296bdfee1905f77baa
eunw00-a <eunw00@snu.ac.kr>
2026-05-14 09:56:39 +0000

    실시간 글자 수 카운터

--- assets/js/hw11.js
+++ assets/js/hw11.js
@@ -2,14 +2,26 @@
 // 2025-11959 이은우
 
 // Q1
-const q1Box = document.querySelector("#q1-box");
+const box = document.querySelector("#q1-box");
 const themeBtn = document.querySelector("#theme-btn");
 themeBtn.addEventListener("click", () => {
-    q1Box.classList.toggle("dark"); 
-    if (q1Box.classList.contains("dark")) {
+    box.classList.toggle("dark"); 
+    if (box.classList.contains("dark")) {
         themeBtn.textContent="라이트 모드";
     }
-    else {
-        themeBtn.textContent="다크 모드";
-    }
+    else {themeBtn.textContent="다크 모드";}
  });
+
+ // Q2
+const input = document.querySelector("#q2-input");
+const count = document.querySelector("#q2-count");
+const warn = document.querySelector("#q2-warn");
+
+input.addEventListener("input", (e) => {
+    count.textContent = e.target.value.length;
+    if (e.target.value.length >= 100) {
+        warn.textContent = "100자를 넘었습니다.";
+        warn.style.color = "crimson";
+    }
+    else {warn.textContent = ""}
+});
