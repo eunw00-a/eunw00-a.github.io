6f740d967a40bd09099e5beb0768e909706b750a
eunw00-a <eunw00@snu.ac.kr>
2026-05-28 14:08:45 +0000

    Q2 노벨문학상 수상자 수 시대별 추이 꺾은선그래프

--- assets/js/hw12.js
+++ assets/js/hw12.js
@@ -7,7 +7,6 @@ fetch("/data/sillok.json")
 .then(records => {
     const labels = records.map(r => r.king);
     const counts = records.map(r => r.volumes);
-    
     const canvas = document.querySelector("#q1-chart");
     new Chart(canvas, {
         type: "bar",
@@ -21,9 +20,50 @@ fetch("/data/sillok.json")
         },
         options: {
             plugins: {
-                title: {display: true, text: "조선왕조실록 왕대별 권수"},
+                title: {display: true, text: "조선왕조실록 왕대별 권수"}
             },
             scales: {y: {beginAtZero: true}}
         }
     });
 });
+
+// Q2
+fetch("/data/nobel-literature.csv")
+    .then(response => response.text())
+    .then(csv => {
+        const rows = csv
+        .split("\n")
+        .slice(1) // 헤더 떼기
+        .filter(line => line.trim() !== "") // 빈 줄 떼기
+        .map(line => {
+            const cols = line.split(",");
+            return {
+                decade: Number(cols[0]),
+                count: Number(cols[1]),
+            };
+        });
+        const labels = rows.map(r => `${r.decade}년대`);
+        const counts = rows.map(r => r.count);
+        const canvas = document.querySelector("#q2-chart");
+        new Chart(canvas, {
+            type: "line",
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: "수상자 수",
+                    data: counts,
+                    borderColor: "rgba(54, 162, 235, 1)",
+                    backgroundColor: "rgba(54, 162, 235, 0.2)" 
+                }]
+            },
+            options: {
+                plugins: {
+                    title: {display: true, text: "노벨문학상 수상자 수 추이 (10년 단위)"}
+                },
+                scales: {
+                x: {title: {display: true, text: "연대"}},
+                y: {title: {display: true, text: "수상자 수"}}
+                }
+            }    
+        });
+    });
